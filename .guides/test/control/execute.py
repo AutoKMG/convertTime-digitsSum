7ad0010debf5917ff71d5a2abc744c66809a9761
Khaled Galal <th.be03@gmail.com>
2022-10-04 10:33:11 +0200

    first commit

---
+++ .guides/test/control/execute.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+""" This is the main runner class that can be invoked to kick off autograding for
+both student pre-submission testing and final submission testing
+"""
+
+from manager import manager
+
+if __name__ == "__main__":
+    m = manager()
